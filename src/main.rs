d0e339eadde7ad2896f44c5c1157dce4fa425f9d
Vitaly Merenkov <vmrnkv@gmail.com>
2020-12-29 15:41:15 +0300

    ability to call as command line

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 use std::str::FromStr;
+use std::{env, process};
 
-use calculator::{Evaluable, Expression};
+use calculator::{Evaluable, EvaluableResult, Expression};
 
 fn main() {
-    match Expression::from_str("2+2") {
-        Ok(e) => println!("{:?}={:?}", e, e.eval()),
-        Err(err) => println!("err {}", err),
+    let mut args = env::args();
+    args.next();
+    let ex = args.next().unwrap_or_else(|| {
+        eprintln!("Type an expression without spaces as an argument, e.g. \"2+2/(5-3)\"");
+        process::exit(1);
+    });
+    let expression = Expression::from_str(&ex).unwrap_or_else(|err| {
+        eprintln!("Problems parsing expression: {}", err);
+        process::exit(1);
+    });
+
+    match expression.eval() {
+        EvaluableResult::F64(v) => println!("{} = {}", ex, v),
+        EvaluableResult::Bool(v) => println!("{} = {}", ex, v),
     }
 }
